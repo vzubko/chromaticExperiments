2fc5b8d09176e2452261fc61b5b4d5a2c9fc13ef
vzubko <varvarazubko@gmail.com>
2016-11-17 15:07:40 +0100

    it is working

--- experiments.py
+++ experiments.py
@@ -8,10 +8,15 @@ import chromaticNumber as cnum
 
 def experiment1(nodes,maxedges):
     clist = []
+    suma = 0
     for edges in range(maxedges+1):
-        graph = graphs.randomGraph(nodes,edges)
-        c = cnum.chromaticNumber(cnum.greedy(graph))
-        clist.append(c)
+        for i in range(10):
+            graph = graphs.randomGraph(nodes,edges)
+            c = cnum.chromaticNumber(cnum.greedy(graph))
+            suma += c
+        average = suma/10
+        suma = 0
+        clist.append(average)
     plt.plot(clist)
     plt.show()
 
