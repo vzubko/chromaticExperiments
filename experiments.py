0e9c8d8937bd6dc866e5a4778f989e0c86b7b53c
vzubko <varvarazubko@gmail.com>
2016-11-17 17:29:00 +0100

    added preferential attachment

--- experiments.py
+++ experiments.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import graphCreator as graphs
 import chromaticNumber as cnum
 
-def listChromatic(nodes,maxedges,avgrepeat,strategy):
+def randomChromatic(nodes, maxedges, avgrepeat, strategy):
     clist = []
     suma = 0
-    for edges in range(maxedges + 1):
+    for edges in range(maxedges+1):
         for i in range(avgrepeat):
             graph = graphs.randomGraph(nodes, edges)
             c = cnum.chromaticNumber(cnum.greedy(graph,strategy))
@@ -20,23 +20,46 @@ def listChromatic(nodes,maxedges,avgrepeat,strategy):
 
 
 def experiment1(nodes,maxedges):
-    l = listChromatic(nodes,maxedges,40,strategy = nx.coloring.strategy_largest_first)
+    l = randomChromatic(nodes, maxedges, 40, nx.coloring.strategy_largest_first)
     plt.plot(l)
     plt.show()
 
 def experiment2(nodes,maxedges, strategies):
     for s in strategies:
-        l = listChromatic(nodes,maxedges,20,strategy = s)
-        plt.plot(l)
+        l = randomChromatic(nodes, maxedges, 3, s[0])
+        plt.plot(l, label=s[1])
+    plt.legend(loc = 0)
     plt.show()
 
-experiment2(60,500, [nx.coloring.strategy_connected_sequential,
-                     nx.coloring.strategy_connected_sequential_bfs,
-                     nx.coloring.strategy_connected_sequential_dfs,
-                     nx.coloring.strategy_independent_set,
-                     nx.coloring.strategy_largest_first,
-                     nx.coloring.strategy_random_sequential,
-                     nx.coloring.strategy_saturation_largest_first,
-                     nx.coloring.strategy_smallest_last])
+
+
+def prefChromatic(nodes,maxedges,avgrepeat,strategy):
+    clist = []
+    suma = 0
+    for edges in range(maxedges):
+        for i in range(avgrepeat):
+            graph = graphs.prefAttach(nodes, edges+1)
+            c = cnum.chromaticNumber(cnum.greedy(graph,strategy))
+            suma += c
+        average = suma / avgrepeat
+        suma = 0
+        clist.append(average)
+    return clist
+
+def experiment3(nodes, maxedges, strategies):
+    for s in strategies:
+        l = prefChromatic(nodes,maxedges,3,s[0])
+        plt.plot(l, label=s[1])
+    plt.legend(loc = 0)
+    plt.show()
+
+experiment3(50,30, [(nx.coloring.strategy_connected_sequential, "connected_sequential"),
+                     (nx.coloring.strategy_connected_sequential_bfs, "connected_sequential_bfs"),
+                     (nx.coloring.strategy_connected_sequential_dfs, "connected_sequential_dfs"),
+                     (nx.coloring.strategy_independent_set, "independent_set"),
+                     (nx.coloring.strategy_largest_first, "largest_first"),
+                     (nx.coloring.strategy_random_sequential, "random_sequential"),
+                     (nx.coloring.strategy_saturation_largest_first, "saturation_largest_first"),
+                     (nx.coloring.strategy_smallest_last, "smallest_last")] )
 
 
--- graphCreator.py
+++ graphCreator.py
@@ -6,3 +6,6 @@ def randomGraph(nodes, edges):
     g = nx.gnm_random_graph(nodes, edges)
     return g
 
+def prefAttach(nodes,edges):
+    g = nx.barabasi_albert_graph(nodes,edges)
+    return g
