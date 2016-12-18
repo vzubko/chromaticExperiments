f7cd3b20f175b5f1709cb2cd5a6c05206da341a6
vzubko <varvarazubko@gmail.com>
2016-12-18 20:14:09 +0100

    update

--- expDegree.py
+++ expDegree.py
@@ -1,17 +1,12 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+
 import graphCreator as graphs
 import chromaticNumber as cnum
-import time
 
-from pyclustering.utils.graph import read_graph, draw_graph
-from pyclustering.samples.definitions import GRAPH_SIMPLE_SAMPLES, GRAPH_DSJC_SAMPLES, FCPS_SAMPLES, IMAGE_REAL_SAMPLES
-
-# graph = GRAPH_DSJC_SAMPLES.DSJC_250_5
 
 graph = graphs.karateClub()
 
 degree = graph.degree()
+print(degree)
 
 
 def getChromaticNumber(numberOfNodes, listOfEdges):
@@ -26,20 +21,38 @@ def getHighestDegreeNodes(graph,number):
     for node, degree in degrees.items():
         if degree >= number:
             degreeList.append(node)
-
     return degreeList
 
 
 def getChromaticWithoutNodes(graph, listOfNodes):
-    cnumber = getChromaticNumber(len(graph.nodes()), graph.edges())
-    print(cnumber)
     edgeList = graph.edges()
     removedEdges = []
     for edge in edgeList:
          if edge[0] not in listOfNodes and edge[1] not in listOfNodes:
             removedEdges.append(edge)
     c = getChromaticNumber(len(graph.nodes()), removedEdges)
-
     return c
+
+
+def getNodesOfOneDegree(graph, number):
+    degrees = graph.degree()
+    degreeList = []
+    for node, degree in degrees.items():
+        if degree == number:
+            degreeList.append(node)
+    return degreeList
+
+def getChromaticRemovedOfOneDegree(graph,listOfNodes):
+    chromaticList = []
+    for node in range(len(listOfNodes)):
+        cNumber = getChromaticWithoutNodes(graph, [listOfNodes[node]])
+        chromaticList.append(cNumber)
+    return chromaticList
+
+print(getChromaticRemovedOfOneDegree(graph, getNodesOfOneDegree(graph,6)))
+
+
+
+
 
 
--- expme.py
+++ expme.py
@@ -4,13 +4,59 @@ import graphCreator as graphs
 import chromaticNumber as cnum
 import time
 
+from pyclustering.utils.graph import read_graph, draw_graph
+from pyclustering.samples.definitions import GRAPH_SIMPLE_SAMPLES, GRAPH_DSJC_SAMPLES, FCPS_SAMPLES, IMAGE_REAL_SAMPLES
 
-graph = graphs.clusterGraph(50,30,0.8)
+# graph = GRAPH_DSJC_SAMPLES.DSJC_250_5
 
-def getChromaticNumber(listOfNodes, listOfEdges):
-    coloring = cnum.getColoringHEA(listOfNodes, listOfEdges)
+graph = graphs.karateClub()
+
+# degree = graph.degree()
+
+repetitions = 20
+
+def getTime(numberOfNodes, listOfEdges):
+    suma = 0
+    for i in range(repetitions):
+        start_time = time.time()
+        cnum.getColoringHEA(numberOfNodes, listOfEdges)
+        end_time = time.time() - start_time
+        suma += end_time
+    average = suma/repetitions
+    return average
+
+
+def getTimeWithoutOneNode(graph,node):
+    edgeList = graph.edges()
+    nodesList = graph.nodes()
+    removedEdges = []
+    for edge in range(len(edgeList)):
+        if node not in edgeList[edge]:
+            removedEdges.append(edgeList[edge])
+    t = getTime(len(nodesList), removedEdges)
+    return t
+
+
+def timing(graph):
+    timeList = []
+    nodesList = graph.nodes()
+    for node in range(len(nodesList)):
+        t = getTimeWithoutOneNode(graph, node)
+        timeList.append(t)
+    return timeList
+
+print(timing(graph))
+
+
+
+
+
+
+def getChromaticNumber(numberOfNodes, listOfEdges):
+    coloring = cnum.getColoringHEA(numberOfNodes, listOfEdges)
     c = max(coloring)
     return c
+
 
 def getCnumberWithoutOneNode(graph,node):
     edgeList = graph.edges()
@@ -22,6 +68,8 @@ def getCnumberWithoutOneNode(graph,node):
     c = getChromaticNumber(len(nodesList), removedEdges)
     return c
 
+# print(getCnumberWithoutOneNode(graph, 2))
+
 def chromaticChanged(graph):
     chromaticList = []
     nodesList = graph.nodes()
@@ -30,14 +78,13 @@ def chromaticChanged(graph):
         chromaticList.append(cNumber)
     return chromaticList
 
-print(chromaticChanged(graph))
+# print(chromaticChanged(graph))
 
-# start_time = time.time()
-#
-# print("Finished in %s seconds " % (time.time() - start_time))
-
-
-
-# pos = nx.spring_layout(graph, iterations=2000)
-# nx.draw(graph, pos, node_color=coloring)
-# plt.show()
+# # start_time = time.time()
+# #
+# # print("Finished in %s seconds " % (time.time() - start_time))
+# #
+# #
+# # pos = nx.spring_layout(graph, iterations=2000)
+# # nx.draw(graph, pos, node_color=coloring)
+# # plt.show()
