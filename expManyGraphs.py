74ad3a7f8b71eea2d62d16ad69dccd8067cea284
vzubko <varvarazubko@gmail.com>
2017-01-12 15:10:13 +0100

    experiments with many graphs

---
+++ expManyGraphs.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import graphCreator as graphs
+import chromaticNumber as cnum
+import random
+
+
+def getChromaticNumber(numberOfNodes, listOfEdges):
+    coloring = cnum.getColoringHEA(numberOfNodes, listOfEdges)
+    c = max(coloring)
+    return c
+
+def graphChromatic(graph):
+    c = getChromaticNumber(len(graph.nodes()), graph.edges())
+    return c
+
+def getChromaticWithoutNodes(graph, listOfNodes):
+    edgeList = graph.edges()
+    removedEdges = []
+    for edge in edgeList:
+         if edge[0] not in listOfNodes and edge[1] not in listOfNodes:
+            removedEdges.append(edge)
+    c = getChromaticNumber(len(graph.nodes()), removedEdges)
+    return c
+
+def getCnumber(graph,listOfNodes):
+    chromaticList = []
+    for node in range(len(listOfNodes)):
+        cNumber = getChromaticWithoutNodes(graph, [listOfNodes[node]])
+        chromaticList.append(cNumber)
+    return chromaticList
+
+def sortChanges(graph, originalChr, listOfNodes):
+    chromaticList = getCnumber(graph, listOfNodes)
+    print(chromaticList)
+    reduced = 0
+    stayed = 0
+    other = 0
+    for cnumber in chromaticList:
+        if cnumber == originalChr-1:
+            reduced += 1
+        elif cnumber == originalChr:
+            stayed += 1
+        else:
+            other += 1
+
+    print ('reduced:', reduced, 'stayed:', stayed, 'other:', other)
+    return reduced, stayed, other
+
+# graph = graphs.randomGraph(8,20)
+# print(sortChanges(graph,graphChromatic(graph),graph.nodes()))
+
+def myrange(upto, offset):
+    c = offset
+    l = []
+    while c < upto:
+        l.append(c)
+        c += 1
+    return l
+
+
+maxCNumber = 10
+reduced = []
+stayed = []
+other = []
+countC = []
+for i in range(maxCNumber):
+    reduced.append(0)
+    stayed.append(0)
+    other.append(0)
+
+
+for i in range(5):
+    print("graph", i)
+    g = graphs.randomGraph(random.randint(5, 10), random.randint(20, 30))
+    originalChr = graphChromatic(g)
+    countC.append(originalChr)
+    a, b, c = sortChanges(g, originalChr, g.nodes())
+    reduced[originalChr] += a
+    stayed[originalChr] += b
+    other[originalChr] += c
+
+print('reduced:', reduced, 'stayed:', stayed, 'other:', other)
+
+
+
+plt.bar(myrange(maxCNumber-0.5, -0.2), reduced, width=0.2, color='green')
+plt.bar(myrange(maxCNumber-0.5, 0), stayed, width=0.2, color='yellow')
+plt.bar(myrange(maxCNumber-0.5, 0.2), other, width=0.2, color='red')
+plt.gca().set_xticks(range(maxCNumber + 1))
+plt.ylabel('Number of nodes')
+plt.xlabel('Chromatic Number')
+plt.title('Change in chromatic number with many random graphs')
+plt.show()
